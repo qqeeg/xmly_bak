583a8876095479b222f13f29973007e568ac1133
qqeeg <532562686@qq.com>
2020-11-24 02:39:37 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -720,7 +720,7 @@ def saveListenTime(cookies, date_stamp):
         'uid': uid
     }
     try:
-        requests.post('http://mobile.ximalaya.com/pizza-category/ball/saveListenTime',
+        response = requests.post('http://mobile.ximalaya.com/pizza-category/ball/saveListenTime',
                       headers=headers, cookies=cookies, data=data)
     except:
         print("网络请求异常,为避免GitHub action报错,直接跳过")
@@ -748,7 +748,7 @@ def listenData(cookies, date_stamp):
         'uid': uid
     }
     try:
-        requests.post('http://m.ximalaya.com/speed/web-earn/listen/client/data',
+        response = ('http://m.ximalaya.com/speed/web-earn/listen/client/data',
                       headers=headers, cookies=cookies, data=json.dumps(data))
     except:
         print("网络请求异常,为避免GitHub action报错,直接跳过")
