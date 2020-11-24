da6a618df5de6c598a38ba75620077d432983438
qqeeg <532562686@qq.com>
2020-11-25 02:30:43 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -58,7 +58,7 @@ if "XMLY_SPEED_COOKIE" in os.environ:
 devices = []
 notify_time = 19                            # 通知时间,24小时制,默认19
 XMLY_ACCUMULATE_TIME = 1                    # 希望刷时长的,此处置1,默认打开;关闭置0
-UserAgent = "ting_1.8.42(MI+8+SE,Android29)"
+UserAgent = "ting_1.8.42(MI+6,Android26)"
 # 非iOS设备的需要的自行修改,自己抓包 与cookie形式类似
 
 
